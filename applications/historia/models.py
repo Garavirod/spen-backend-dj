d66690c24491bfc5ad165248c707ffc459115c62
Garavirod <rodrigogarciaavila26@gmail.com>
2021-03-09 13:02:10 -0600

    Valoraciones, Historias and Comentarios Models were added

--- applications/historia/models.py
+++ applications/historia/models.py
@@ -4,6 +4,9 @@ from model_utils.models import TimeStampedModel
 from applications.escritor.models import Usuarios
 
 class Historias(TimeStampedModel):
+    """ 
+        Este calse define la estructura del modelo Historias,        
+    """
     titulo = models.CharField(
         'titulo',
         max_length=200,
@@ -18,7 +21,8 @@ class Historias(TimeStampedModel):
 
     genero = models.CharField(
         'genero',
-        max_length=20
+        max_length=20,
+        default=''
     )
 
     sinopsis = models.TextField(
@@ -32,9 +36,16 @@ class Historias(TimeStampedModel):
         default=''
     )
 
-    status = models.BooleanField('terminada', default=False)
+    status = models.BooleanField(
+        'terminada', 
+        default=False
+    )
 
-    autor = models.ForeignKey(Usuarios,on_delete=models.CASCADE)
+    # Un autor puede tener muchas historias
+    autor = models.ForeignKey(
+        Usuarios,
+        on_delete=models.CASCADE
+    )
 
     class Meta:
         verbose_name = 'Historia'
@@ -42,3 +53,47 @@ class Historias(TimeStampedModel):
 
     def __str__(self):
         return self.titulo
+
+class Comentarios(TimeStampedModel):
+    """ 
+        Este calse define la estructura del modelo cometario,        
+    """
+    contenido = models.TextField(
+        'contenido',
+        max_length=500
+    )
+
+    # Un autor puede hacer multiples comentarios
+    autor_comentario = models.ForeignKey(
+        Usuarios,
+        on_delete=models.CASCADE
+    )
+
+    # Una historia pude tener muchos comentarios y viceversa
+    historia_comentario = models.ManyToManyField(Historias)
+
+    class Meta:
+        verbose_name = 'Comentario'
+        verbose_name_plural = 'comentarios'
+
+    def __str__(self):
+        return str(self.pk)
+
+class Valoraciones(TimeStampedModel):
+    """ 
+        Esta clase define la estructura del modelo valoración  
+    """
+    puntaje = models.PositiveIntegerField('puntaje', max_length=1, default=0)
+
+    # Un usuario pude hacer multiples valoraciuones
+    autor_valoracion = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
+
+    # Una historai tiene muchas valoraciones
+    historia_valoracion = models.ForeignKey(Historias, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Valoración'
+        verbose_name_plural = 'Valoraciones'
+
+    def __str__(self):
+        return str(self.puntaje)
